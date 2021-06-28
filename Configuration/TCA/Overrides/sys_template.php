3dff4b22c712fd260760857062374009182c2e95
MathiasSchaller <mathias.schaller@gmx.at>
2021-06-28 18:15:25 +0200

    TCA Overrides

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+'lieferant',
+'Configuration/TypoScript',
+'Main Template'
+)
+?>
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,14 @@
+<?php
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Plan2Net.lieferant',
+    'LieferantList',
+    'Lieferantliste',
+    'EXT:lieferant/Resources/Public/Icons/lieferant_domain_model_category.svg'
+);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Plan2Net.lieferant',
+    'LieferantDetail',
+    'LieferantDetail',
+    'EXT:lieferant/Resources/Public/Icons/lieferant_domain_model_category.svg'
+);
